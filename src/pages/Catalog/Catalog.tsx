de4bce23eda13b884ec9e30fc65119d684f0c093
Oleksandr <gayrsuk@gmail.com>
2024-10-09 00:15:25 +0300

    init project

---
+++ src/pages/Catalog/Catalog.tsx
@@ -0,0 +1,7 @@
+import css './Catalog.module.css'
+
+const Catalog = () => {
+  return <div>Catalog</div>;
+};
+
+export default Catalog;
---
+++ src/pages/Favorites/Favorites.tsx
@@ -0,0 +1,7 @@
+import css from './Favorites.module.css';
+
+const Favorites = () => {
+  return <div>Favorites</div>;
+};
+
+export default Favorites;
