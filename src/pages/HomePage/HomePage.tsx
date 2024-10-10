9eafb7ed1dcd30c9f5a126d5f7dd440a4756ffaf
Oleksandr <gayrsuk@gmail.com>
2024-10-10 23:08:26 +0300

    Added Home page layout

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,15 @@
 import css from './HomePage.module.css';
 
 const HomePage = () => {
-  return <div className={css.homepage}>HomePage</div>;
+  return (
+    <div className={css.homepage}>
+      <div className={css.hero_block}>
+        <h1>Campers of your dreams</h1>
+        <h2>You can find everything you want in our catalog</h2>
+        <button className={css.button}>View Now</button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
