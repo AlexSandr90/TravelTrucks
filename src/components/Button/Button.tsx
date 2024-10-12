e9065740505062801403a63502c8e0d21d2bbc25
Oleksandr <gayrsuk@gmail.com>
2024-10-13 00:21:37 +0300

    Added Load more btn

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,12 +3,14 @@ import css from './Button.module.css';
 const Button = ({
   children,
   onClick,
+  className,
 }: {
   children: React.ReactNode;
   onClick: () => void;
+  className?: string;
 }) => {
   return (
-    <button className={css.button} onClick={onClick}>
+    <button className={`${css.button} ${className}`} onClick={onClick}>
       {children}
     </button>
   );
--- src/components/CampersList/CampersList.tsx
+++ src/components/CampersList/CampersList.tsx
@@ -1,3 +1,4 @@
+import Button from '../Button/Button';
 import CamperItem from '../CamperItem/CamperItem';
 import css from './CampersList.module.css';
 
@@ -5,6 +6,12 @@ const CampersList = () => {
   return (
     <article className={css.campers_list__wrapper}>
       <CamperItem />
+      <Button
+        className={css.load_more__btn}
+        onClick={() => console.log('Load more')}
+      >
+        Load more
+      </Button>
     </article>
   );
 };
